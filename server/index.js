ceaf941a73bf9cd6d68231344ebc5da502613ffc
Maël SUARD <maelsuard@orange.fr>
2024-02-18 12:52:59 +0100

    cors solved

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
-require('dotenv').config();
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
-const cors = require('cors');
-const winston = require('winston');
+require("dotenv").config();
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const cors = require("cors");
+const winston = require("winston");
 
 const app = express();
 const port = process.env.SERVER_PORT || 3001;
@@ -11,36 +11,35 @@ const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
     origin: "*",
-    methods: ["GET", "POST"]
-  }
+    methods: ["GET", "POST"],
+  },
 });
+
+app.use(cors()); // Apply CORS middleware here
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 require("./start/db")();
 require("./start/routes")(app);
 
-
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.use((err, req, res, next) => {
   winston.error(err.message, err);
-  res.status(500).send('Something failed.');
+  res.status(500).send("Something failed.");
 });
 
-io.on('connection', (socket) => {
-  winston.info('a user connected');
-  socket.on('disconnect', () => {
-    winston.info('user disconnected');
+io.on("connection", (socket) => {
+  winston.info("a user connected");
+  socket.on("disconnect", () => {
+    winston.info("user disconnected");
   });
 });
 
-server.listen(port, () => {  
+server.listen(port, () => {
   winston.info(`Server is running at http://localhost:${port}`);
-})
+});
 
 module.exports = io;
