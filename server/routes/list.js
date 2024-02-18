09d3db94ced40045aedbd43f5c6307f3f764a278
Ting WANG <skywy000@gmail.com>
2024-02-18 17:33:32 +0100

    comment router.post

--- server/routes/list.js
+++ server/routes/list.js
@@ -8,6 +8,7 @@ router.get("/", async (req, res) => {
   res.send(shoppingLists);
 });
 
+/*
 router.post("/", async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
@@ -21,7 +22,7 @@ router.post("/", async (req, res) => {
 
   wss.broadcast({ type: "shoppingListAdded", data: newShoppingList });
   res.send(newShoppingList);
-});
+});*/
 
 router.put("/:id", async (req, res) => {
   const { error } = validate(req.body);
