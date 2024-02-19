c2b8318d6b7473e6f7de7143020ae9d198a2fa3c
Mael29000 <maelsuard@orange.fr>
2024-02-19 10:38:37 +0100

    responsive 3

--- src/components/Participants.tsx
+++ src/components/Participants.tsx
@@ -4,40 +4,42 @@ import { getUsers } from "../services";
 import { User } from "./types";
 
 export default function Participants() {
-  const [participants, setParticipants] = React.useState<User[]>([]);
+    const [participants, setParticipants] = React.useState<User[]>([]);
 
-  React.useEffect(() => {
-    getUsers().then((users) => {
-      setParticipants(users.map((user: any) => user.username));
-    });
-  }, []);
+    React.useEffect(() => {
+        getUsers().then((users) => {
+            setParticipants(users.map((user: any) => user.username));
+        });
+    }, []);
 
-  return (
-    <div>
-      <h2 style={{ fontSize: "20px", textAlign: "center" }}>Participants</h2>
-      <div style={{ padding: "20px 50px" }}>
-        {participants.map((participant) => (
-          <ParticipantItem key={participant} name={participant} />
-        ))}
-      </div>
-    </div>
-  );
+    return (
+        <div>
+            <h2 style={{ fontSize: "20px", textAlign: "center" }}>
+                Participants
+            </h2>
+            <div style={{ padding: "10px 50px" }}>
+                {participants.map((participant) => (
+                    <ParticipantItem key={participant} name={participant} />
+                ))}
+            </div>
+        </div>
+    );
 }
 
 interface ParticipantItemProps {
-  name: string;
+    name: string;
 }
 
 const ParticipantItem = (props: ParticipantItemProps) => {
-  const { name } = props;
+    const { name } = props;
 
-  return (
-    <p
-      style={{
-        marginBottom: "20px",
-      }}
-    >
-      {name}
-    </p>
-  );
+    return (
+        <p
+            style={{
+                marginBottom: "15px",
+            }}
+        >
+            {name}
+        </p>
+    );
 };
--- src/components/Repartition.tsx
+++ src/components/Repartition.tsx
@@ -4,58 +4,64 @@ import { getRepartition } from "./mocks";
 import { useRepartition } from "../contexts/RepartitionContext";
 
 export default function Repartition() {
-  const { repartition: repartitionContext } = useRepartition();
+    const { repartition: repartitionContext } = useRepartition();
 
-  const [repartition, setRepartition] =
-    React.useState<Part[]>(repartitionContext);
+    const [repartition, setRepartition] =
+        React.useState<Part[]>(repartitionContext);
 
-  React.useEffect(() => {
-    setRepartition(repartitionContext);
-  }, [repartitionContext]);
+    React.useEffect(() => {
+        setRepartition(repartitionContext);
+    }, [repartitionContext]);
 
-  return (
-    <div>
-      <h2 style={{ fontSize: "20px", textAlign: "center" }}>Répartition</h2>
-      <div style={{ padding: "10px 50px" }}>
-        {repartition.map((item) => (
-          <RepartitionItem
-            key={item.user}
-            name={item.user}
-            value={item.total}
-          />
-        ))}
-      </div>
-    </div>
-  );
+    return (
+        <div>
+            <h2 style={{ fontSize: "20px", textAlign: "center" }}>
+                Répartition
+            </h2>
+            <div style={{ padding: "0px 50px" }}>
+                {repartition.map((item) => (
+                    <RepartitionItem
+                        key={item.user}
+                        name={item.user}
+                        value={item.total}
+                    />
+                ))}
+            </div>
+        </div>
+    );
 }
 
 interface RepartitionItemProps {
-  name: string;
-  value: number;
+    name: string;
+    value: number;
 }
 
 const RepartitionItem = (props: RepartitionItemProps) => {
-  const { name, value } = props;
+    const { name, value } = props;
 
-  return (
-    <div
-      style={{
-        display: "grid",
-        gridTemplateColumns: "1fr 1fr",
-        alignItems: "center",
-      }}
-    >
-      <p>{name}</p>
-      <p
-        style={{
-          textAlign: "end",
-          fontWeight: "700",
-          color: "rgba(0,0,0,0.66)",
-          fontSize: "20px",
-        }}
-      >
-        {value} €
-      </p>
-    </div>
-  );
+    return (
+        <div
+            style={{
+                display: "grid",
+                gridTemplateColumns: "1fr 1fr",
+                alignItems: "center",
+            }}
+        >
+            <p>{name}</p>
+            <p
+                style={{
+                    textAlign: "end",
+                    fontWeight: "700",
+                    color: "rgba(0,0,0,0.66)",
+                    fontSize: "20px",
+                }}
+            >
+                {formatPrice(value)} €
+            </p>
+        </div>
+    );
+};
+
+const formatPrice = (price: number) => {
+    return price.toFixed(2).replace(".", ",");
 };
